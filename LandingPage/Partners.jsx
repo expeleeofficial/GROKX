4a5a40f2f1a397ef94417a29f2687c3ff083bd58
divyesh123-jain <divyeshjain318@gmail.com>
2023-12-13 23:23:20 +0530

    added partners

--- LandingPage/Partners.jsx
+++ LandingPage/Partners.jsx
@@ -45,7 +45,7 @@ const Partners = () => {
    <div className="mx-auto max-w-7xl py-12 px-6 lg:px-8 lg:py-24">
      <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
        <div className="space-y-5 sm:space-y-4">
-         <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Meet our Partners</h2>
+         <h2 className="text-3xl text-center font-bold tracking-tight sm:text-4xl">Meet our Partners</h2>
         
        </div>
        <div className="lg:col-span-2">
